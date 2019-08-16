74e6bf8cfbdbea66915593ad8e9b0e84c389b229
sd1700092 <foreverskyone@hotmail.com>
2019-08-16 08:00:26 +0800

    practice Scala Impatient2 chapter10

--- src/main/scala/impatient2/chapter3/Main.scala
+++ src/main/scala/impatient2/chapter3/Main.scala
@@ -7,5 +7,15 @@ object Main {
     val random = new Random
     val a = for (i <- 0 until 5) yield random.nextInt(5)
     println(a)
+
+    val arr = Array(1, 2, 3, 4, 5)
+    for (i <- 0 to arr.length) {
+      if (i % 2 == 0 && i < arr.length - 1) {
+        val tmp = arr(i + 1)
+        arr(i + 1) = arr(i)
+        arr(0) = tmp
+      }
+    }
+    println(arr.mkString(" ").toString)
   }
 }
